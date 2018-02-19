1be92a93b6bcb28dea73d4be413589f58d1ea92f
Kelly Miller <kellyannemiller@gmail.com>
2018-02-19 15:33:37 -0700

    finish phase 2

--- scripts.js
+++ scripts.js
@@ -2,10 +2,17 @@ var userInput = document.getElementById('user-input');
 var guessBtn = document.getElementById('guess-btn');
 var lastGuess = document.getElementById('user-guess');
 var clearBtn = document.getElementById('clear-btn');
+var hint = document.getElementById('hint');
+var resetBtn = document.getElementById('reset-btn')
+var userGuess;
+var secretNumber;
+
+window.onload = createSecretNumber()
 
 guessBtn.addEventListener('click', function(){
-  console.log('hit!', userInput.value);
+  userGuess = parseInt(userInput.value);
   lastGuess.innerHTML = userInput.value;
+  compareNumbers()
   clearInput();
 })
 
@@ -13,6 +20,41 @@ clearBtn.addEventListener('click', function(){
   clearInput();
 })
 
+resetBtn.addEventListener('click', function(){
+  clearInput();
+  createSecretNumber();
+  disableBtns();
+  lastGuess.innerHTML = '';
+  hint.innerHTML = '';
+})
+
+userInput.addEventListener('keyup', function(){
+  guessBtn.disabled = false;
+  clearBtn.disabled = false;
+  resetBtn.disabled = false;
+})
+
 function clearInput() {
   userInput.value = '';
+}
+
+function disableBtns(){
+  guessBtn.disabled = true;
+  clearBtn.disabled = true;
+  resetBtn.disabled = true;
+}
+
+function createSecretNumber(){
+  secretNumber = Math.floor(Math.random() * (100-0) + 0);
+  console.log(secretNumber)
+}
+
+function compareNumbers(){
+  if(userGuess < secretNumber){
+    hint.innerHTML = 'too low!';
+  } else if (userGuess > secretNumber){
+    hint.innerHTML = 'too high!';
+  } else {
+    hint.innerHTML = 'boom!';
+  }
 }
